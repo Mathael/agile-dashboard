6b8a7646c1b0e28c21f17876985ed5f1afc023a3
LEBOC Philippe <LebocPhilippe@live.be>
2017-12-30 15:00:44 +0100

    feat(model): impl model repositories

--- dashboard-core/src/main/java/com/dashboard/core/repository/TicketTimeSpentRepository.java
+++ dashboard-core/src/main/java/com/dashboard/core/repository/TicketTimeSpentRepository.java
@@ -1,6 +1,6 @@
 package com.dashboard.core.repository;
 
-import com.dashboard.core.model.project.Project;
+import com.dashboard.core.model.ticket.TicketTimeSpent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
  * @author Leboc Philippe.
  */
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Integer> {
+public interface TicketTimeSpentRepository extends JpaRepository<TicketTimeSpent, Integer> {
 }
