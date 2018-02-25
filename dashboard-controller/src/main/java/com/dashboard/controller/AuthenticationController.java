322e0cb1b65bbcfd4a7f4c88048190ecc2643820
LEBOC Philippe <LebocPhilippe@live.be>
2018-02-25 23:23:48 +0100

    feat(controller): introduce auth controller

---
+++ dashboard-controller/src/main/java/com/dashboard/controller/AuthenticationController.java
@@ -0,0 +1,30 @@
+package com.dashboard.controller;
+
+import com.dashboard.api.bean.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+/**
+ * @author Leboc Philippe.
+ */
+@RestController
+@RequestMapping(value = "/auth", produces = APPLICATION_JSON_UTF8_VALUE)
+public class AuthenticationController {
+
+    @ResponseStatus(code = HttpStatus.CREATED)
+    @RequestMapping(value = "/register", method = POST)
+    public Response register() {
+        return new Response();
+    }
+
+    @RequestMapping(value = "/login", method = GET)
+    public Response login() {
+        return new Response();
+    }
+}
