4c8ae60e7378b15aadb7d476f1074bf101273045
LEBOC Philippe <philippe.leboc@plebicom.com>
2018-02-28 14:03:05 +0100

    feat(interceptor): add demo interceptor

---
+++ dashboard-controller/src/main/java/com/dashboard/interceptor/DefaultInterceptor.java
@@ -0,0 +1,32 @@
+package com.dashboard.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author LEBOC Philippe
+ */
+@Slf4j
+@Component
+public class DefaultInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        // Do nothing
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        // Do nothing
+    }
+}
