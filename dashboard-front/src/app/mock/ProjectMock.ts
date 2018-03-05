d0620b9b8acfaeb69a7a49cdaff1c70ee5f480bc
LEBOC Philippe <philippe.leboc@plebicom.com>
2018-03-05 13:29:33 +0100

    fix(mock): wrong data on sprint & tickets

--- dashboard-front/src/app/mock/ProjectMock.ts
+++ dashboard-front/src/app/mock/ProjectMock.ts
@@ -5,7 +5,7 @@ export class ProjectMock {
     static getData() : Project[] {
         return [
             new Project(1, 'Angular 5 POC', 'Establishing a POC of angular concept applyed to a realworld project for a realworld deploy', [], [], [], []),
-            new Project(2, 'Spring', 'Establishing a POC of angular concept applyed to a realworld project for a realworld deploy', [], [{id: 100, label: 'impl authentication', description: null, estimatedTime: 6}, {id: 101, label: 'impl bean validation', description: null, estimatedTime: 16}], [], []),
+            new Project(2, 'Spring', 'Establishing a POC of angular concept applyed to a realworld project for a realworld deploy', [], [{id: 2525, label: 'first sprint !', description: 'Il faut tout finir !!!', tickets: [{id: 100, label: 'impl authentication', description: null, estimatedTime: 6}, {id: 101, label: 'impl bean validation', description: null, estimatedTime: 16}]}], [], []),
             new Project(3, 'Spring Security', 'Establishing a POC of angular concept applyed to a realworld project for a realworld deploy', [], [], [], []),
             new Project(4, 'Spring OAuth2', 'Establishing a POC of angular concept applyed to a realworld project for a realworld deploy', [], [], [], []),
             new Project(5, 'Spring Cache', 'Establishing a POC of angular concept applyed to a realworld project for a realworld deploy', [], [], [], []),
